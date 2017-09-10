6e52f724ddd786263836e8f4a9d92ae5daddc3a9
Joseph Duffy <josephduffy@me.com>
2017-09-10 15:06:17 +0100

    Updates classes to conform to new Consequences types
    Updates `consequences` and `@types/node` to latest versions
    Adds `preferGlobal` to `package.json`

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { EventEmitter } from 'events';
 
 export default class DateTimeInitialiser implements AddonInitialiser {
 
-  public readonly name = 'Date Time';
-
-  public readonly description = 'Provides the system\'s date and time';
+  public readonly metadata = {
+    name: 'Date Time',
+    description: 'Provides the system\'s date and time',
+    supportsMultipleInstances: false,
+  };
 
   public createInstance(metadata: Addon.Metadata, configOptions?: { [id: string]: any; }): Promise<DateTime> {
     return Promise.resolve(new DateTime(metadata));
@@ -17,15 +19,15 @@ export class DateTime implements Addon {
 
   public metadata: Addon.Metadata;
 
-  get variables(): Promise<Variable[]> {
-    return Promise.resolve([this.dateTime]);
-  }
-
   private dateTime: DateTimeVariable;
 
   constructor(metadata: Addon.Metadata) {
     this.metadata = metadata;
     this.dateTime = new DateTimeVariable();
+  }
+
+  public async loadVariables(): Promise<Variable[]> {
+    return [this.dateTime];
   }
 
 }
@@ -36,14 +38,14 @@ class DateTimeVariable extends EventEmitter implements Variable {
 
   public readonly name = 'System Date and Time';
 
-  get currentValue(): Date {
-    return new Date();
-  }
-
   private timer: NodeJS.Timer;
 
   constructor() {
     super();
+  }
+
+  public async retrieveValue(): Promise<Date> {
+    return new Date();
   }
 
   public addChangeEventListener(listener: () => void) {
